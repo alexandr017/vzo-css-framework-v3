6c3e30b277c83ea7979c75169fcb94c4d7a8da92
alex <sasha17@live.ru>
2021-04-26 10:07:08 +0300

    remove php files inside modules

--- inc-framework/loader.php
+++ inc-framework/loader.php
@@ -6,21 +6,49 @@
 function import($path)
 {
     global $mode;
+
+    $html = '';
+    $css = '';
+
     if ($mode == 'amp') {
-        if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/modules/'  . $path . '/amp.php')) {
-            include $_SERVER['DOCUMENT_ROOT'] . '/modules/' . $path . '/amp.php';
-        } elseif (file_exists($_SERVER['DOCUMENT_ROOT'] . '/modules/'  . $path . '/mob.php')) {
-            include $_SERVER['DOCUMENT_ROOT'] . '/modules/'  .  $path . '/mob.php';
-        } elseif (file_exists($_SERVER['DOCUMENT_ROOT'] . '/modules/'  . $path . '/pc.php')) {
-            include $_SERVER['DOCUMENT_ROOT'] . '/modules/'  .  $path . '/pc.php';
+        if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/modules/'  . $path . '/amp.html')) {
+            $html = loader($_SERVER['DOCUMENT_ROOT'] . '/modules/' . $path . '/amp.html');
+            $css = loader($_SERVER['DOCUMENT_ROOT'] . '/modules/' . $path . '/amp.css');
+        } elseif (file_exists($_SERVER['DOCUMENT_ROOT'] . '/modules/'  . $path . '/mob.html')) {
+            $html = loader($_SERVER['DOCUMENT_ROOT'] . '/modules/' . $path . '/mob.html');
+            $css = loader($_SERVER['DOCUMENT_ROOT'] . '/modules/' . $path . '/mob.css');
+        } elseif (file_exists($_SERVER['DOCUMENT_ROOT'] . '/modules/'  . $path . '/pc.html')) {
+            $html = loader($_SERVER['DOCUMENT_ROOT'] . '/modules/' . $path . '/pc.html');
+            $css = loader($_SERVER['DOCUMENT_ROOT'] . '/modules/' . $path . '/pc.css');
         }
     } elseif ($mode == 'mob') {
-        if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/modules/' . $path . '/mob.php')) {
-            include $_SERVER['DOCUMENT_ROOT'] . '/modules/' . $path . '/mob.php';
-        } elseif (file_exists($_SERVER['DOCUMENT_ROOT'] . '/modules/' . $path . '/pc.php')) {
-            include $_SERVER['DOCUMENT_ROOT'] . '/modules/' . $path . '/pc.php';
+        if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/modules/' . $path . '/mob.html')) {
+            $html = loader($_SERVER['DOCUMENT_ROOT'] . '/modules/' . $path . '/mob.html');
+            $css = loader($_SERVER['DOCUMENT_ROOT'] . '/modules/' . $path . '/mob.css');
+        } elseif (file_exists($_SERVER['DOCUMENT_ROOT'] . '/modules/' . $path . '/pc.html')) {
+            $html = loader($_SERVER['DOCUMENT_ROOT'] . '/modules/' . $path . '/pc.html');
+            $css = loader($_SERVER['DOCUMENT_ROOT'] . '/modules/' . $path . '/pc.css');
         }
-    } elseif(file_exists($_SERVER['DOCUMENT_ROOT'] . '/modules/' . $path . '/pc.php')) {
-        include $_SERVER['DOCUMENT_ROOT'] . '/modules/' . $path . '/pc.php';
+    } elseif(file_exists($_SERVER['DOCUMENT_ROOT'] . '/modules/' . $path . '/pc.html')) {
+        $html = loader($_SERVER['DOCUMENT_ROOT'] . '/modules/' . $path . '/pc.html');
+        $css = loader($_SERVER['DOCUMENT_ROOT'] . '/modules/' . $path . '/pc.css');
     }
+
+    echo $html;
+    echo "<style>$css</style>";
+}
+
+/**
+ * @param string $path
+ * @return string $code
+ */
+function loader($path)
+{
+    $code = '';
+
+    if (file_exists($path)) {
+        $code = file_get_contents($path);
+    }
+
+    return $code;
 }
